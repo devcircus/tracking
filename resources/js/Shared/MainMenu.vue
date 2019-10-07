859bb26673b9e958a330a72d9bca2e2523989088
Clayton Stone <devcircus@outlook.com>
2019-10-07 15:08:58 -0500

    Replace a few svg icons.

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -2,7 +2,7 @@
     <div :class="display">
         <div :class="margin">
             <inertia-link class="flex items-center group py-3" :href="route('dashboard')">
-                <icon-base :dynamic-classes="isPath('dashboard') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="dashboard" classes="mr-2">
+                <icon-base :width="20" :height="20" :dynamic-classes="isPath('dashboard') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="dashboard" classes="mt-1 mr-2">
                     <dashboard />
                 </icon-base>
                 <div :class="isPath('dashboard') ? 'text-blue-200' : 'text-white group-hover:text-blue-200'" class="text-xl">Dashboard</div>
@@ -10,7 +10,7 @@
         </div>
         <div :class="margin">
             <inertia-link class="flex items-center group py-3" :href="route('inventory')">
-                <icon-base :dynamic-classes="isPath('inventory') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="book" classes="mr-2">
+                <icon-base :dynamic-classes="isPath('inventory') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="book" classes="mt-1 mr-2">
                     <book />
                 </icon-base>
                 <div :class="isPath('inventory') ? 'text-blue-200' : 'text-white group-hover:text-blue-200'" class="text-xl">Inventory</div>
@@ -18,7 +18,7 @@
         </div>
         <div :class="margin">
             <inertia-link class="flex items-center group py-3" :href="route('reports.list')">
-                <icon-base :dynamic-classes="isPath('reports') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="inbox-full" classes="mr-2">
+                <icon-base :dynamic-classes="isPath('reports') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="inbox-full" classes="mt-1 mr-2">
                     <inbox-full />
                 </icon-base>
                 <div :class="isPath('reports') ? 'text-blue-200' : 'text-white group-hover:text-blue-200'" class="text-xl">Reports</div>
@@ -26,7 +26,7 @@
         </div>
         <div v-if="$page.auth.user.is_admin || $page.auth.user.is_artist" :class="margin">
             <inertia-link class="flex items-center group py-3" :href="route('artwork.list')">
-                <icon-base :dynamic-classes="isPath('artwork') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="pen-tool" classes="mr-2">
+                <icon-base :dynamic-classes="isPath('artwork') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="pen-tool" classes="mt-1 mr-2">
                     <pen-tool />
                 </icon-base>
                 <div :class="isPath('artwork') ? 'text-blue-200' : 'text-white group-hover:text-blue-200'" class="text-xl">Artwork</div>
@@ -34,7 +34,7 @@
         </div>
         <div>
             <inertia-link class="flex items-center group py-3" :href="route('materials.list')">
-                <icon-base :dynamic-classes="isPath('materials') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="printer" classes="mr-2">
+                <icon-base :dynamic-classes="isPath('materials') ? 'fill-blue-300' : 'fill-white group-hover:fill-blue-300'" icon-name="printer" classes="mt-1 mr-2">
                     <printer />
                 </icon-base>
                 <div :class="isPath('materials') ? 'text-blue-200' : 'text-white group-hover:text-blue-200'" class="text-xl">Materials</div>
